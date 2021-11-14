6ed9b4271cefb3cc0067e93bc974e5f7f5be092d
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2021-11-14 11:50:56 -0500

    start setting up timer internal structures

--- src/components/timers/Countdown.js
+++ src/components/timers/Countdown.js
@@ -1,40 +1,32 @@
 import React, { useContext } from "react";
 import PropTypes from "prop-types";
 
+import { AppContext } from "../../context/AppProvider";
+import { useInterval } from "../../utils/customReactHooks";
+
 import { H1 } from "../../utils/tokensAndTheme";
 import Button, { ButtonSpacer } from "../generic/Button";
 import DisplayTime from "../generic/DisplayTime";
 import TimeInput, { TimeInputLabel } from "../generic/TimeInput";
-import { AppContext } from "../../context/AppProvider";
 
 
 
 const Countdown = (props) => {
-  const { onStart } = props;
-
-  const { hours, setHours, minutes, setMinutes, seconds, setSeconds } = useContext(AppContext);
+  const { handleSetHours, handleSetMinutes, handleSetSeconds, handleStop, handleStart, handleReset } = props;
+  const { hours, setHours, minutes, setMinutes, seconds, setSeconds, isTimerRunning } = useContext(AppContext);
   
-  const updateHours = (e) => {
-    console.log("updateHours", e.target.value);
-    setHours(e.target.value);
+  function countdown() {
+    if (seconds) {
+      setSeconds(1 + parseInt(seconds));
+    } else {
+      setSeconds(1);
+    }
+    
   }
 
-  const updateMinutes = (e) => {
-    console.log("updateMinutes", e.target.value);
-    setMinutes(e.target.value);
-  }
-
-  const updateSeconds = (e) => {
-    console.log("updateSeconds", e.target.value);
-    setSeconds(e.target.value);
-  }
-
-  function onReset (e) {
-    console.log("clicked!", e);
-    setHours(null);
-    setMinutes(null);
-    setSeconds(null);
-  }
+  useInterval(() => {
+    countdown();
+  }, isTimerRunning ? 1000 : null);
 
   return (
     <React.Fragment>
@@ -42,17 +34,26 @@ const Countdown = (props) => {
       <DisplayTime {...{ hours, minutes, seconds}} />
       <TimeInputLabel>
         Start Time:
-        <TimeInput hoursVal={hours} minutesVal={minutes} secondsVal={seconds} onChangeHours={updateHours} onChangeMinutes={updateMinutes} onChangeSeconds={updateSeconds} />
+        <TimeInput disabled={isTimerRunning} hoursVal={hours} minutesVal={minutes} secondsVal={seconds} onChangeHours={handleSetHours} onChangeMinutes={handleSetMinutes} onChangeSeconds={handleSetSeconds} />
       </TimeInputLabel>
       <ButtonSpacer>
-        <Button onClick={onStart}>START</Button>
-        <Button onClick={onReset} variant="secondary">RESET</Button>
+        { isTimerRunning ?
+          <Button onClick={handleStop} variant="danger">STOP</Button>:
+          <Button onClick={handleStart}>START</Button>
+        }
+        <Button onClick={handleReset} variant="secondary">RESET</Button>
       </ButtonSpacer>
     </React.Fragment>
   );
 }
 Countdown.propTypes = {
   onStart: PropTypes.func,
+  handleSetHours: PropTypes.func,
+  handleSetMinutes: PropTypes.func,
+  handleSetSeconds: PropTypes.func,
+  handleStop: PropTypes.func,
+  handleStart: PropTypes.func,
+  handleReset: PropTypes.func,
 }
 
 export default Countdown;
--- src/components/timers/Stopwatch.js
+++ src/components/timers/Stopwatch.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
+
+import { AppContext } from "../../context/AppProvider";
+import { useInterval } from "../../utils/customReactHooks";
 
 import { H1 } from "../../utils/tokensAndTheme";
 import Button, { ButtonSpacer } from "../generic/Button";
 import DisplayTime from "../generic/DisplayTime";
 
 const Stopwatch = (props) =>  {
-  const { onStart, onReset } = props;
+  const { handleSetHours, handleSetMinutes, handleSetSeconds, handleStop, handleStart, handleReset } = props;
+  const { hours, setHours, minutes, setMinutes, seconds, setSeconds, isTimerRunning } = useContext(AppContext);
+  
+  function stopwatch() {
+    if (typeof seconds === "string" && !seconds) { // empty string
+      setSeconds(1);
+    } else {
+      setSeconds(1 + parseInt(seconds));
+    }
+    
+  }
+
+  useInterval(() => {
+    stopwatch();
+  }, isTimerRunning ? 1000 : null);
+
   return (
     <React.Fragment>
       <H1>Stopwatch</H1>
-      <DisplayTime />
+      <DisplayTime {...{ hours, minutes, seconds}} />
       <ButtonSpacer>
-        <Button onClick={onStart}>START</Button>
-        <Button onClick={onReset} variant="secondary">RESET</Button>
+        { isTimerRunning ?
+          <Button onClick={handleStop} variant="danger">STOP</Button>:
+          <Button onClick={handleStart}>START</Button>
+        }
+        <Button onClick={handleReset} variant="secondary">RESET</Button>
       </ButtonSpacer>
     </React.Fragment>
   );
 }
 Stopwatch.propTypes = {
-  onStart: PropTypes.func,
-  onReset: PropTypes.func,
+  handleSetHours: PropTypes.func,
+  handleSetMinutes: PropTypes.func,
+  handleSetSeconds: PropTypes.func,
+  handleStop: PropTypes.func,
+  handleStart: PropTypes.func,
+  handleReset: PropTypes.func,
 }
 
 export default Stopwatch;
