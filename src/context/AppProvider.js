b39e699323eea79e939beb26c48304bb1768a2b6
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2021-11-16 23:47:50 -0500

    use array for timer state; related re-org + fixes

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -3,42 +3,41 @@ import React, { useState } from 'react';
 export const AppContext = React.createContext({});
 
 const AppProvider = ({ children }) => {
+  const emptyTimer = ["", "", ""]; // [Hours, Minutes, Seconds]
+
+  // State shared across all timers
+  const [ timer, setTimer ] = useState(emptyTimer);
   const [ timerIdx, setTimerIdx ] = useState(0); // Timer array specified in TimersView.js
-  const [ startTime, setStartTime ] = useState(["", "", ""]); // [hours/minutes/seconds]
-  const [ endTime, setEndTime ] = useState(["", "", ""]);
-  const [ workTime, setWorkTime ] = useState(["", "", ""]);
-  const [ restTime, setRestTime ] = useState(["", "", ""]);
-  const [ hours, setHours ] = useState("");
-  const [ minutes, setMinutes ] = useState("");
-  const [ seconds, setSeconds ] = useState("");
-  const [ numRounds, setNumRounds ] = useState(1);
-  const [ currRound, setCurrRound ] = useState(1);
   const [ isTimerRunning, setTimerRunning ] = useState(false);
   const [ isIncrementing, setIsIncrementing ] = useState(true); // if False, is decrementing
+  // States specific to certain timers
+  const [ startTime, setStartTime ] = useState(emptyTimer); // Used in Countdown & XY
+  const [ endTime, setEndTime ] = useState(emptyTimer); // Used in Stopwatch
+  const [ workTime, setWorkTime ] = useState(emptyTimer); // Used in Tabata
+  const [ restTime, setRestTime ] = useState(emptyTimer); // Used in Tabata
+  const [ numRounds, setNumRounds ] = useState(1); // Used in Tabata and XY
+  const [ currRound, setCurrRound ] = useState(1); // Used in Tabata and XY
   const [ isWorkTime, setIsWorkTime ] = useState(true); // if False, is rest time
 
-  const { 0: startHours, 1: startMinutes, 2: startSeconds } = startTime || [];
-  const { 0: workHours, 1: workMinutes, 2: workSeconds } = workTime || [];
-  const { 0: restHours, 1: restMinutes, 2: restSeconds } = restTime || [];
-  const { 0: endHours, 1: endMinutes, 2: endSeconds } = endTime || [];
+  const { 0: hours, 1: minutes, 2: seconds } = timer || [];
+  // console.log("timer", timer, hours, minutes, seconds);
 
   /* Counting up the seconds from 00:00:00 to endTime */
   const tickUp = () => {
+    const { 0: endHours = 0, 1: endMinutes = 0, 2: endSeconds = 0 } = endTime || [];
     if (
-      seconds === endSeconds && 
-      minutes === endMinutes &&
-      hours === endHours
+      (seconds || 0) === endSeconds && 
+      (minutes || 0) === endMinutes &&
+      (hours || 0) === endHours
     ) {
       timerComplete();
     } else {
       if (seconds === 59) {
-        setSeconds(0);
-        setMinutes((minutes || 0) + 1);
+        setTimer([hours, (minutes || 0) + 1, 0]);
       } else if (minutes === 59) {
-        setMinutes(0);
-        setHours((hours || 0) + 1);
+        setTimer([(hours || 0) + 1, 0, seconds]);
       } else {
-        setSeconds((seconds || 0) + 1);
+        setTimer([hours, minutes, (seconds || 0) + 1]);
       }
     }
   }
@@ -49,12 +48,10 @@ const AppProvider = ({ children }) => {
     alert('Timer complete!');
   }
 
-  const roundComplete = () => { // Tabata
+  const roundComplete = () => {
     if (currRound !== numRounds) {
       // start a new round
-      setHours(startHours);
-      setMinutes(startMinutes);
-      setSeconds(startSeconds);
+      setTimer(startTime);
       setCurrRound(currRound + 1);
     } else {
       timerComplete();
@@ -66,24 +63,12 @@ const AppProvider = ({ children }) => {
       timerComplete();
     } else {
       if (isWorkTime) {
-        // update counter with rest times
-        setHours(restHours);
-        setMinutes(restMinutes);
-        setSeconds(restSeconds);
-
-        // convert to rest period
-        setIsWorkTime(false);
+        setTimer(restTime); // update counter with rest time
+        setIsWorkTime(false); // convert to rest period
       } else {
-        // update counter with work times
-        setHours(workHours);
-        setMinutes(workMinutes);
-        setSeconds(workSeconds);
-
-        // convert to work period
-        setIsWorkTime(true);
-
-        // start a new round
-        setCurrRound(currRound + 1);
+        setTimer(workTime); // update counter with work times
+        setIsWorkTime(true); // convert to work period
+        setCurrRound(currRound + 1); // start a new round
       }
     }
   }
@@ -91,19 +76,21 @@ const AppProvider = ({ children }) => {
   /* Counting down the seconds from start time to 00:00:00 */
   const tickDown = (onCompleteCallback) => {
     if (!hours && !minutes && !seconds) {
+      // console.log("wow it's done", hours, minutes, seconds);
       onCompleteCallback();
     } else if (!minutes && !seconds) {
-      setHours(hours - 1);
-      setMinutes(59);
-      setSeconds(59);
+      // console.log("hour done", hours - 1, 59, 59);
+      setTimer([(hours - 1), 59, 59]);
     } else if (!seconds) {
-      setMinutes(minutes - 1);
-      setSeconds(59);
+      // console.log("minute done", hours, minutes-1, 59);
+      setTimer([hours, (minutes - 1), 59]);
     } else {
-      setSeconds(seconds - 1);
+      // console.log("second done", hours, minutes, seconds-1);
+      setTimer([hours, minutes, (seconds - 1)]);
     }
   }
 
+  // Event Handlers
   const handleSetStartTime = (e) => {
     handleSetTimeInput(e, setStartTime, startTime);
   }
@@ -149,23 +136,19 @@ const AppProvider = ({ children }) => {
   }
 
   const handleStart = (e) => {
+    // console.log("handling start");
     if (isIncrementing) {
       // specify end time
     } else {
       if (timerIdx !== 3) { // XY or Countdown
         // start at start time
-        setHours(startHours);
-        setMinutes(startMinutes);
-        setSeconds(startSeconds);
+        // console.log("startTime", startTime);
+        setTimer(startTime);
       } else { // Tabata
         if (isWorkTime) {
-          setHours(workHours);
-          setMinutes(workMinutes);
-          setSeconds(workSeconds);
+          setTimer(workTime);
         } else {
-          setHours(restHours);
-          setMinutes(restMinutes);
-          setSeconds(restSeconds);
+          setTimer(restTime);
         }
       }
     }
@@ -174,9 +157,7 @@ const AppProvider = ({ children }) => {
 
   const handleReset = (e) => {
     handleStop();
-    setHours("");
-    setMinutes("");
-    setSeconds("");
+    setTimer(emptyTimer);
     setCurrRound(1);
     setIsWorkTime(true);
   }
@@ -184,19 +165,17 @@ const AppProvider = ({ children }) => {
   return (
     <AppContext.Provider
       value={{
-        startTime,
-        endTime,
         hours,
         minutes,
         seconds,
+        startTime,
+        endTime,
         numRounds,
         currRound,
         isTimerRunning,
         setStartTime,
         setEndTime,
-        setHours,
-        setMinutes,
-        setSeconds,
+        setTimer,
         setNumRounds,
         setCurrRound,
         setTimerRunning,
